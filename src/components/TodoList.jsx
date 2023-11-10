3f4aca5025dcfdeb53dd88127931ccd46d07059d
Vic-Fernandes07 <vitoria.ronchi7@gmail.com>
2023-11-10 17:00:15 -0300

    Co-authored-by: Caitto <s4tarcay@users.noreply.github.com> Co-authored-by: Sophia  <loreen-s23@users.noreply.github.com>

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,11 +1,53 @@
-import  styles from './TodoList.module.css'
+import React, { useState } from "react";
+import styles from "./TodoList.module.css";
 
-const TodoList = () => {
+
+function TodoList() {
+  const [input, setInput] = useState("");
+  const [todo, setTodo] = useState([]);
+  const [buttonState, setButtonState] = useState(false);
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    if (input.trim() !== "") {
+      setTodo([...todo, input]);
+      setInput("");
+    }
+  };
+
+  const handleButtonClick = () => {
+    setButtonState(!buttonState);
+    // console.log(buttonState);
+  };
+
   return (
-    <div>
-      <h1 className={styles.h1}>ola</h1>
+    <div className={styles.text}>
+      <h1>To Do List</h1>
+      <form onSubmit={addTodo}>
+        <input
+          type="text"
+          placeholder="Enter new task"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button type="submit">Adicionar tarefa</button>
+      </form>
+      <ul>
+        {todo.map((todo, index) => (
+          <li key={index}>
+            <button
+              className={`${styles.button_green} ${
+                  buttonState ? styles.verde : styles.branco
+                }`}
+                onClick={handleButtonClick}
+                ></button>
+                {todo}
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default TodoList
+
+export default TodoList;
