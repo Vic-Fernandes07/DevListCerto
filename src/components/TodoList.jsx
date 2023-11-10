4345b33059f992d4cd005f0d2a3d5ee7856fbd4b
Vic-Fernandes07 <vitoria.ronchi7@gmail.com>
2023-11-10 08:56:49 -0300

    inicio

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,11 @@
+import  styles from './TodoList.module.css'
+
+const TodoList = () => {
+  return (
+    <div>
+      <h1 className={styles.h1}>ola</h1>
+    </div>
+  )
+}
+
+export default TodoList
